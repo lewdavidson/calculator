7f70c4e6308cdc63d5734834e4b7da52643da9b4
Lew Davidson <lewdavidson47@gmail.com>
2017-10-16 16:55:36 -0700

    add extra prompt answer

---
+++ extra/extra.js
@@ -0,0 +1,8 @@
+var information = function (name, age, food) {
+  return name + age + food;
+};
+
+var name = prompt("Enter your name:");
+var age = prompt("Enter your age:");
+var food = prompt("Enter your favorite food")
+var information = alert("Hello, " + name + "I see you are " + age + "! " + "I also like to eat " + food + ".");
